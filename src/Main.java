e91499f3e0ddf5ea796ae63bb5ba64ef5864d1ca
M0L0D0Y <i7ascha@gmail.com>
2022-04-01 21:42:26 +0300

    сделал метод добавления траты как сказали - без расчетов.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,6 @@ import java.math.BigDecimal;
 public class Main {
 
     public static void main(String[] args) {
-        SessionManager sessionManager = new SessionManager();
-        sessionManager.addPersons("pavel");
-        sessionManager.addPersons("Kolya");
-        sessionManager.addPersons("Vasiliy");
-        sessionManager.addExpence("bar", "pavel", BigDecimal.valueOf(900));
-        sessionManager.printExpence();
     }
 }
 
--- src/service/SessionManager.java
+++ src/service/SessionManager.java
@@ -17,28 +17,7 @@ public class SessionManager {
         personList.add(person);
     }
 
-    public void addExpence(String name, String from, BigDecimal amount) {// передаем имя траты, кто заплатил и сколько
-        List<Person> listDebtor = new ArrayList<>(personList);//чтобы не удалять из сессии создаем копию
-        Person payer = null;
-        for (int i=0; i< listDebtor.size(); i++){//ищем по имени
-            if (listDebtor.get(i).getName().equals(from)){//как находим
-                payer = listDebtor.get(i);// получаем его
-                listDebtor.remove(payer);// и удаляем из списка
-                break;// здесь не уверен в правильности досрочного выхода из цикла
-            }
-        }
-        BigDecimal number = BigDecimal.valueOf(personList.size());//узнаем сколько было участников
-        BigDecimal duty = amount.divide(number);//узнаем по сколько должны были все заплатить
-        // может сделать какое-то округление?
-        for (Person person : listDebtor) {//создаем трату для каждого из должников
-            Expence expence = new Expence(generateId.generate(), name, payer, person, duty);
-            expenceList.add(expence);//записываем ее в список трат
-        }
+    public void addExpence(String name, Person from, Person to, BigDecimal amount) {
+        expenceList.add(new Expence(generateId.generate(), name, from, to, amount));
     }
-    public void printExpence(){
-        for (Expence expence: expenceList){
-            System.out.println(expence);
-        }
-    }
-
 }
