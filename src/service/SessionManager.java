462a2d726e37a734e6366ea40791e9e39c87218e
M0L0D0Y <i7ascha@gmail.com>
2022-04-02 10:49:44 +0300

    привязался к id

--- src/service/SessionManager.java
+++ src/service/SessionManager.java
@@ -20,4 +20,53 @@ public class SessionManager {
     public void addExpence(String name, Person from, Person to, BigDecimal amount) {
         expenceList.add(new Expence(generateId.generate(), name, from, to, amount));
     }
+    public void printExpence() {
+        calculateExpence();
+        if (expenceList.isEmpty()) {
+            System.out.println("Никто никому не должен.");
+            return;
+        }
+        for (Expence expence : expenceList) {
+            System.out.println(expence.getTo() + " должен " + expence.getFrom() + " " + expence.getAmount());
+        }
+    }
+
+    private void calculateExpence() {
+        for (Person person : personList) {
+            List<Long> listName = new ArrayList<>();
+            for (Expence item : expenceList) {
+                listName.add(item.getFrom().getId());
+            }
+            while (listName.contains(person.getId())) {
+                listName.remove(person.getId());
+                for (int i = 0; i < expenceList.size(); i++) {
+                    long name = expenceList.get(i).getFrom().getId();
+                    for (int j = 0; j < expenceList.size(); j++) {
+                        if (name == (expenceList.get(j).getTo().getId())) {
+                            long name1 = expenceList.get(j).getFrom().getId();
+                            if (name1 == (expenceList.get(i).getTo().getId())) {
+                                BigDecimal amount = expenceList.get(i).getAmount();
+                                BigDecimal amount1 = expenceList.get(j).getAmount();
+                                int value = amount.compareTo(amount1);
+                                if (value > 0) {
+                                    BigDecimal newAmount = amount.subtract(amount1);
+                                    expenceList.get(i).setAmount(newAmount);
+                                    expenceList.remove(expenceList.get(j));
+                                } else if (value < 0) {
+                                    BigDecimal newAmount1 = amount1.subtract(amount);
+                                    expenceList.get(j).setAmount(newAmount1);
+                                    expenceList.remove(expenceList.get(i));
+                                } else {
+                                    Expence expence1 = expenceList.get(i);
+                                    Expence expence = expenceList.get(j);
+                                    expenceList.remove(expence1);
+                                    expenceList.remove(expence);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
